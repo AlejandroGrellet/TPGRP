1c71770d0703d33debf2e8fece1363fcca7db1a5
AleGrellet <mbmnubia@gmail.com>
2020-10-14 18:03:03 -0300

    Agregado de ArrayLists

--- TP4/src/principal/controladores/ControladorPrincipal.java
+++ TP4/src/principal/controladores/ControladorPrincipal.java
@@ -6,6 +6,13 @@
 package principal.controladores;
 
 import autores.modelos.Alumnos;
+import autores.modelos.Profesores;
+import grupos.modelos.grupos;
+import idiomas.modelos.Idioma;
+import java.util.ArrayList;
+import lugares.modelos.lugar;
+import palabrasclave.modelos.palabraclave;
+import tipos.modelos.tipos;
 
 /**
  *
@@ -13,8 +20,27 @@ import autores.modelos.Alumnos;
  */
 public class ControladorPrincipal {
     public static void main(String[] args) {
-        Alumnos UnAlumno = new Alumnos(1,"Pérez","Ana","1234","5555");
-        UnAlumno.mostrar();
+        ArrayList<Alumnos> ListaAlumnos = new ArrayList();
+        ArrayList<Profesores> ListaProfesores = new ArrayList();
+        ArrayList<Idioma> ListaIdiomas = new ArrayList();
+        ArrayList<grupos> ListaGrupos = new ArrayList();
+        ArrayList<palabraclave> ListaPalClave = new ArrayList();
+        ArrayList<tipos> ListaTipos = new ArrayList();
+        ArrayList<lugar> ListaLugares = new ArrayList();
+        Alumnos AL1=new Alumnos(1, "asd", "fgh", "2"," 3");
+        Alumnos AL2=new Alumnos(1, "asd", "fgh", "2"," 3");
+        Alumnos AL3=new Alumnos(1, "asd", "fgh", "2"," 3");
+        Alumnos AL4=new Alumnos(1, "asd", "fgh", "2"," 3");
+        Alumnos AL5=new Alumnos(1, "Rodriguez", "fgh", "2"," 3");
+        ListaAlumnos.add(AL1);
+        ListaAlumnos.add(AL2);
+        ListaAlumnos.add(AL3);
+        ListaAlumnos.add(AL4);
+        ListaAlumnos.add(AL5);
+        for (Alumnos AL : ListaAlumnos) {
+            AL.mostrar();
+        }
+        
     }
     
 }
